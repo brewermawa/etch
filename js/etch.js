a7a3bd46b9632aa61bd8d9c095aed90b1c0cd41f
Mawa Guerra <brewermawa@gmail.com>
2023-01-11 18:16:43 -0600

    Done!

--- js/etch.js
+++ js/etch.js
@@ -1,7 +1,7 @@
 const canvasWidth = 600;
-const numberOfSquares = 20;
-const width = Math.round(canvasWidth / numberOfSquares);
-const height = Math.round(canvasWidth / numberOfSquares);
+let numberOfSquares = 20;
+let width = Math.round(canvasWidth / numberOfSquares);
+let height = Math.round(canvasWidth / numberOfSquares);
 let rainbow = 0;
 console.log("arriba");
 
@@ -9,7 +9,31 @@ let color = "black";
 let canvas = document.querySelector(".canvas");
 let square;
 
-for (let i = 1; i <= numberOfSquares ** 2; i++) {
+createCanvas();
+
+function createCanvas () {
+    for (let i = 1; i <= numberOfSquares ** 2; i++) {
+        square = document.createElement("div");
+        square.id = "sq" + i;
+        square.classList.add("square");
+        square.style.width = width + "px";
+        square.style.height = height + "px";
+        square.addEventListener("mouseenter", (e) => {
+            if (rainbow === 1) {
+                let r = Math.floor(Math.random() * 255);
+                const g = Math.floor(Math.random() * 255);
+                const b = Math.floor(Math.random() * 255);
+                color = `rgb(${r},${g},${b})`
+            }
+            e.target.style.backgroundColor = color;
+            console.log(e.target);
+        });
+        
+        canvas.appendChild(square);
+    }
+}
+
+/* for (let i = 1; i <= numberOfSquares ** 2; i++) {
     square = document.createElement("div");
     square.id = "sq" + i;
     square.classList.add("square");
@@ -27,7 +51,7 @@ for (let i = 1; i <= numberOfSquares ** 2; i++) {
     });
     
     canvas.appendChild(square);
-}
+} */
 
 document.querySelector("#black").addEventListener("click", () => { color = "black"; rainbow = 0; });
 document.querySelector("#white").addEventListener("click", () => { color = "white"; rainbow = 0; });
@@ -45,6 +69,11 @@ let size = document.querySelector(".size");
 let range = document.querySelector("#myRange")
 console.log("abajo");
 
-range.addEventListener("input", () =>{
+range.addEventListener("input", () => {
     size.textContent = range.value + " x " + range.value;
+    numberOfSquares = parseInt(range.value);
+    width = Math.round(canvasWidth / numberOfSquares);
+    height = Math.round(canvasWidth / numberOfSquares);
+    canvas.innerHTML = "";
+    createCanvas();
 });
